176add53e2451f0badf8f1e0321fcf7b9db641ae
KwameCode <kwameogh@gmail.com>
2016-04-25 00:33:30 +0000

    adding methods to test

--- phpUnitTest.php
+++ phpUnitTest.php
@@ -7,4 +7,34 @@ class Test extends PHPUnit_Framework_TestCase {
 		$expected = '{"result":1, "Text":"done"}';
 		$this->assertTrue(true, $url, $expected);
 	}
+
+	public function testsaveDescription() {
+		$url = "equipmentajax.php?cmd=1&txtName=change&equipID=5858";
+		$expected = '{"result":1, "Text":"done"}';
+		$this->assertTrue(true, $url, $expected);
+	}
+
+	public function testsaveStatus() {
+		$url = "equipmentajax.php?cmd=1&txtName=change&equipID=5858";
+		$expected = '{"result":1, "Text":"done"}';
+		$this->assertTrue(true, $url, $expected);
+	}
+
+	public function testsaveCategory() {
+		$url = "equipmentajax.php?cmd=1&txtName=change&equipID=5858";
+		$expected = '{"result":1, "Text":"done"}';
+		$this->assertTrue(true, $url, $expected);
+	}
+
+	public function testsavePrice() {
+		$url = "equipmentajax.php?cmd=1&txtName=change&equipID=5858";
+		$expected = '{"result":1, "Text":"done"}';
+		$this->assertTrue(true, $url, $expected);
+	}
+
+	public function testsaveManufacturer() {
+		$url = "equipmentajax.php?cmd=1&txtName=change&equipID=5858";
+		$expected = '{"result":1, "Text":"done"}';
+		$this->assertTrue(true, $url, $expected);
+	}
 }
