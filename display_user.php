3879ed980eb53a55298a0893af25aa7fafbf7fe9
benedictabempah <benedicta.bempah@ashesi.edu.gh>
2016-05-04 21:08:17 +0000

    Added files via upload

---
+++ display_user.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <title>Display Users</title>
+      <link rel="stylesheet" href="display.css">
+      <link href="bootstrap/bootstrap.min.css" rel="stylesheet">
+      <script type="text/javascript" src="jquery-2.1.3.js"></script>
+      
+</head> 
+<body>
+<?php
+		/**
+		*Include the view_equipment class and use an instance of it to display equipment information
+		*/
+	include_once("users.php");
+	
+		$obj = new users();
+		$obj->getUsers();		
+		$row = $obj->fetch();
+		if (!$row){
+			echo "Unavailable data";
+		}
+		else{
+		
+			echo ("<table class='table table-condensed table-hover table-bordered'>");
+			echo("<tr><th>Student ID</th>
+				  <th>Firstname</th>
+				  <th>Lastname</th> "); 
+					
+			while ($row) {
+			
+									
+				echo "<tr>";
+
+		
+				echo "<td>{$row['Student_ID']}</td>";
+				
+				echo "<td>{$row['First_name']}</td>";
+				
+				echo "<td>{$row['Last_name']}</td>";
+
+				echo "</tr>";
+				$row = $obj->fetch();
+			
+		}
+	}
+
+			echo ("</table>");
+		
+?>
+</body> 
+</html>
